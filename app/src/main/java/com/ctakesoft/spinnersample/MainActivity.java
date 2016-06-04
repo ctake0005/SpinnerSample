63db2f28d67e7bd688d84a0fd69cd70296aa795b
takenach <takenach@gmail.com>
2016-06-04 21:38:24 +0900

    Sample of "Spinner" for the range selection

--- app/src/main/java/com/ctakesoft/spinnersample/MainActivity.java
+++ app/src/main/java/com/ctakesoft/spinnersample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.ctakesoft.spinnersample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner_entry);
+        final TextView under = (TextView) findViewById(R.id.under_text);
+        final TextView upper = (TextView) findViewById(R.id.upper_text);
+        if (spinner == null || under == null || upper == null) {
+            return;
+        }
+
+        final View button = findViewById(R.id.getBtn);
+        if (button != null) {
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String value = (String) spinner.getSelectedItem();
+                    String[] values = value.split("~");
+                    under.setText(values[0].trim());
+                    if (values.length > 1) {
+                        upper.setText(values[1].trim());
+                    } else {
+                        upper.setText("");
+                    }
+                }
+            });
+        }
     }
 }
